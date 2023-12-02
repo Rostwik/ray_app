f90c437dcf7d8fa1849c1d530c79f73b65ad01e9
rostw <rostwik@gmail.com>
2023-12-02 19:13:24 +0300

    Added model display in the admin panel

--- diary_project/diary/admin.py
+++ diary_project/diary/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from diary.models import DiaryEntry
+
+
+@admin.register(DiaryEntry)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'date', 'event', 'feelings'
+    ]
